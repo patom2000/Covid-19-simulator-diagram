e141e8a2fbdfdab2e56c796969a5e0a4f9bdf9bf
Punpetch Prakongpak <56551527+Blackross22@users.noreply.github.com>
2022-02-23 23:52:25 +0700

    :sparkles: add mobility
    add mobility for each node

--- project.cc
+++ project.cc
@@ -19,51 +19,73 @@ class Student{
 	Ipv4InterfaceContainer interface;
 	ApplicationContainer app;
 	CsmaHelper csma;
+	MobilityHelper mobility;
     	Student(int number, int datarate, int delay){
     		node.Create(number);
     		stack.Install(node);
     		devices = csma.Install(node);
-			void setip(nethost, netmask);
-			void setcsma(datarate, delay);
-			void setrecvport(number);
-			void setsentport(number);
-
-    		
+			setip(nethost, netmask);
+			setcsma(datarate, delay);
+			setrecvport(number);
+			setsentport(number);
+			move(node);
+    		 
     	};
-		void Student::setcsma(int datarate, int delay){
+		void setcsma(int datarate, int delay){
 			//set csma
 			csma.SetChannelAttribute ("DataRate", DataRateValue (DataRate (datarate)));
   			csma.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (delay)));
 		}
 
-		void Student::setip(string nethost, string netmask){
+		void setip(string nethost, string netmask){
 			//set IPV4
     		address.SetBase (Ipv4Address(nethost.c_str()), Ipv4Mask(netmask.c_str()));
     		interface = address.Assign(devices);
 		}
 		
-		void Student::setsentport(int amount){
+		void setsentport(int amount){
 			OnOffHelper onoff ("ns3::UdpSocketFactory", Address (InetSocketAddress (Ipv4Address ("255.255.255.255"), port)));
 			onoff.SetConstantRate (DataRate ("500kb/s"));
 			for (int node_id = 0; node_id < amount; node_id++) {
-  				app.Add (onoff.Install (node.Get (node_id)));)
+  				app.Add (onoff.Install (node.Get (node_id)));
 			}
-			setstartstoptime(1, 10);
+			time(1, 20);
 		}
 
-		void Student::time(float start, float stop){
+		void time(float start, float stop){
 			app.Start (Seconds (start));
 			app.Stop (Seconds (stop));
 		}
 
-		void Student::setrecvport(int amount){
+		void setrecvport(int amount){
 			PacketSinkHelper sink ("ns3::UdpSocketFactory",
             Address (InetSocketAddress (Ipv4Address::GetAny (), port)));
 			for (int node_id = 0; node_id < amount; node_id++) {
 			app = sink.Install (node.Get (node_id));
 			}
-			setstartstoptime(1, 2);
+			time(1, 20);
 
+		}
+		static void CourseChange (std::string context, Ptr<const MobilityModel> mobility)
+		{
+		Vector pos = mobility->GetPosition ();
+		Vector vel = mobility->GetVelocity ();
+		std::cout << Simulator::Now () << ", model=" << mobility << ", POS: x=" << pos.x << ", y=" << pos.y
+					<< ", z=" << pos.z << "; VEL:" << vel.x << ", y=" << vel.y
+					<< ", z=" << vel.z << std::endl;
+		}
+		void move(NodeContainer c){
+			mobility.SetPositionAllocator ("ns3::RandomDiscPositionAllocator",
+                                 "X", StringValue ("100.0"),
+                                 "Y", StringValue ("100.0"),
+                                 "Rho", StringValue ("ns3::UniformRandomVariable[Min=0|Max=100]"));
+  			mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
+                             "Mode", StringValue ("Time"),
+                             "Time", StringValue ("2s"),
+                             "Speed", StringValue ("ns3::ConstantRandomVariable[Constant=1.0]"),
+                             "Bounds", StringValue ("0|200|0|200"));
+  			mobility.Install (c);
+  			Config::Connect ("/NodeList/*/$ns3::MobilityModel/CourseChange", MakeCallback (&CourseChange));
 		}
 	uint16_t port = 9;
 };
@@ -71,6 +93,7 @@ class Student{
 
 int main(int argc, char *argv[]){
     Student(5, 5000000, 2);
+	Simulator::Stop (Seconds (20.0));
     AnimationInterface anim ("project.xml");
     Simulator::Run ();
     Simulator::Destroy ();
